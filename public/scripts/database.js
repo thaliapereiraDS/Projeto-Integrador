f24f61635d5d785819390b3cea2715dbad0ebdc6
Renan Bueno de Camargo Castro <50303949821@>
2024-12-11 16:59:26 -0300

    11/12

---
+++ public/scripts/database.js
@@ -0,0 +1,194 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto:1,
+        tituloProduto:"Câmera EOS Rebel SL3 com Lente EF-S 18-55mm", 
+        preco: 4499.00,
+        descricao:"A DSLR mais leve do mundo, com tela articulável - a EOS SL3 é portátil e excelente para elevar as suas fotos e vídeos a um novo nível. Fácil, agradável de utilizar e intuitiva permite a você registrar os grandes momentos da vida com mais criatividade. Com vídeos em 4K, sensor de 24,1 megapixels e Wi-Fi e do Bluetooth®², que lhe permitem estabelecer facilmente ligação ao seu dispositivo inteligente e compartilhar os resultados à medida que fotografa.",
+        imagemProduto: {
+            img1:"./images/products/prod01/cam1.jpg",
+            img2:"./images/products/prod01/cam2.jpg",
+            img3:"./images/products/prod01/cam3.jpg",
+            img4:"./images/products/prod01/cam4.jpg",
+            img4:"./images/products/prod01/cam5.jpg",
+
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:2,
+        tituloProduto:"Câmera EOS R100 (Corpo)", 
+        preco: 3799.00, 
+        descricao:"Transforme Momentos em Memórias. Experimente a diversão da criação de conteúdo de montagem RF com a menor e mais leve câmera da série EOS R, Obtenha uma profundidade de campo incrível com um sensor com uma dimensão 4 vezes superior à dos melhores smartphones. Não precisa de truques para conseguir um fundo desfocado e efeitos criativos na câmara.",
+        imagemProduto: {
+            img1:"./images/products/prod02/Cam1.jpg",
+            img2:"./images/products/prod02/Cam2.jpg",
+            img3:"./images/products/prod02/Cam3.jpg",
+            img4:"./images/products/prod02/Cam4.jpg",
+    },
+        categoriaProduto: 'cameras',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:3,
+        tituloProduto:"Câmera EOS R100 com Lente RF-S 18-45mm", 
+        preco: 7999.00, 
+        descricao:"Capture os momentos importantes sem esforço e com detalhes extraordinários: o grande sensor de imagem APS-C de 24.1 MP proporciona um melhor desempenho de captação de luz e um maior controle sobre a profundidade de campo. A EOS R100 está equipada com um sensor de imagem CMOS (APS-C) de 24.1 milhões de pixels e processador de imagem DIGIC 8. ",
+        imagemProduto: {
+            img1:"./images/products/prod03/cam1.jpg",
+            img2:"./images/products/prod03/cam2.jpg",
+            img3:"./images/products/prod03/cam3.jpg",
+            img4:"./images/products/prod03/cam4.jpg",
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:4,
+        tituloProduto:"Câmera EOS R8 com lente RF 24-50mm f/4.5-6.3 IS STM", 
+        preco: 12100.00, 
+        descricao:"Grande em funcionalidades, pequena em tamanhos. A EOS R8 é a nossa câmera mirrorless Full-Frame mais leve, pesando apenas 461g. É sua melhor aliada para capturar momentos incríveis.",
+        imagemProduto: {
+            img1:"./images/products/prod04/cam1.jpg",
+            img2:"./images/products/prod04/cam2.jpg",
+            img3:"./images/products/prod04/cam3.jpg",
+            img4:"./images/products/prod04/cam4.jpg",
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:5,
+        tituloProduto:"Canon PowerShot SX70 HS", 
+        preco: 6499.00, 
+        descricao:"Capture momentos especiais em grande estilo com a PowerShot SX70 HS, uma câmera compacta Super Zoom de 65X. Caracterizada por seu alcance impressionante, a PowerShot SX70 HS da Canon é uma câmera compacta flexível que oferece imagens de alta resolução e uma lente zoom de amplo alcance de 65x. ",
+        imagemProduto: {
+            img1:"./images/products/prod05/cam1.jpg",
+            img2:"./images/products/prod05/cam2.jpg",
+            img3:"./images/products/prod05/cam3.jpg",
+            img4:"./images/products/prod05/cam4.jpg",
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:6,
+        tituloProduto:"EOS R50 com Lente RF-S 18-45mm", 
+        preco: 300.00, 
+        descricao:"A Canon EOS R50 é uma câmera mirrorless de entrada, ideal para quem busca qualidade de imagem excepcional, recursos avançados e um design compacto e fácil de usar. Com a lente RF-S 18-45mm, este kit é perfeito para iniciantes e entusiastas da fotografia e vídeo, proporcionando flexibilidade para capturar desde paisagens amplas até retratos e closes detalhados.",
+        imagemProduto: {
+            img1:"./images/products/prod06/cam1.jpg",
+            img2:"./images/products/prod06/cam2.jpg",
+            img3:"./images/products/prod06/cam3.jpg",
+            img4:"./images/products/prod06/cam4.jpg",
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:7,
+        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        preco: 5899.00, 
+        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        imagemProduto: {
+            img1:"./images/products/prod02/img1.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:8,
+        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        preco: 300.00, 
+        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        imagemProduto: {
+            img1:"./images/products/prod02/img1.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:9,
+        tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+        preco: 300.00, 
+        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        imagemProduto: {
+            img1:"./images/products/prod02/img1.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:10,
+        tituloProduto:"Promoções", 
+        preco: 300.00, 
+        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        imagemProduto: {
+            img1:"./images/products/prod02/img1.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true
+    },
+    {
+        codigoProduto:11,
+        tituloProduto:"Mais vendidos", 
+        preco: 300.00, 
+        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        imagemProduto: {
+            img1:"./images/products/prod02/img1.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:11,
+        tituloProduto:"Mais vendidos", 
+        preco: 300.00, 
+        descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+        imagemProduto: {
+            img1:"./images/products/prod02/img1.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Cameras',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+]
